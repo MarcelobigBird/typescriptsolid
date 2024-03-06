7b1e5412a7d2f330466661f14d52dabef02ac151
MarcelobigBird <marcelomuckeiro2022@gmail.com>
2024-03-05 22:48:39 -0300

    Vantagens, desvantagens e DRY, YAGNI, KISS, SOC, MVP

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ *  Vantagens e desvantagens do S.O.L.I.D.
+ *
+ *  Vantagens:
+ * - Código modular
+ * - Código reutilizável (D.R.Y - Dont't repeat yourself)
+ * - Código testável, baixo acoplamento
+ * - Baixo acoplamento e alto coesão
+ * - Código expansível
+ * - Separation of concerns (Separação de conceitos)
+ * - Fácil manutenção
+ *
+ *  Desvantagens:
+ * - Complexidade
+ * - Quantidade de código digitado aumenta
+ * - Tempo de desenvolvimento aumenta bastante
+ * - Tenha cuidados com: YAGNI, KISS (You aren't gonna need it, Keep it simple, stupid!)
+ *
+ */
